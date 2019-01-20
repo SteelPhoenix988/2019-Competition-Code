03262ef193fa96618d219238ae26cc1494c123b9
SteelPhoenix988 <frcrobotics988@gmail.com>
2019-01-20 10:40:23 -0800

    Unrolling my changes from the test commit

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -11,7 +11,8 @@ package frc.robot;
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-public class OI {
+public class OI 
+{
   //// CREATING BUTTONS
   // One type of button is a joystick button which is any button on a
   //// joystick.
@@ -39,12 +40,5 @@ public class OI {
   // Start the command when the button is released and let it run the command
   // until it is finished as determined by it's isFinished method.
   // button.whenReleased(new ExampleCommand());
-public static void what()
-{
-  for(int i = 0; i < 2012; ++i)
-  {
-    System.out.println("Hey");
-  }
-}
 
 }
