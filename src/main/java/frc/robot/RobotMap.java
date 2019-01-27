ff03d439e4b7c1a381302fc8198512307e57101f
FOODFINDER1 <alexbean@cox.net>
2019-01-27 11:46:14 -0800

    Following Guide

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,62 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+  // For example to map the left and right motors, you could define the
+  // following variables to use with your drivetrain subsystem.
+  // public static int leftMotor = 1;
+  // public static int rightMotor = 2;
+
+  // If you are using multiple modules, make sure to define both the port
+  // number and the module. For example you with a rangefinder:
+  // public static int rangefinderPort = 1;
+  // public static int rangefinderModule = 1;
+  
+
+  public static final int mFL = 2;
+  public static final int mRL = 3;
+  public static final int mFR = 1;
+  public static final int mRR = 0;
+
+  public static final int ControlPort = 0;
+
+  public MecanumDrive m_robotDrive;
+  public Joystick m_stick;
+  
+  
+  
+  
+  
+  
+  public void robotInit() {
+    VictorSP frontLeft = new VictorSP(mFL);
+    VictorSP rearLeft = new VictorSP(mRL);
+    VictorSP frontRight = new VictorSP(mFR);
+    VictorSP rearRight = new VictorSP(mRR);
+
+    // Invert the left side motors.
+    // You may need to change or remove this to match your robot.
+    frontLeft.setInverted(true);
+    rearLeft.setInverted(true);
+
+    m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+
+    m_stick = new Joystick(ControlPort);
+  }
+}
